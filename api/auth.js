9ec75d5bd823f6f4512070c6d3b35823be8d7372
Michał Dziuba <michaldziuba.praca@gmail.com>
2022-11-29 09:04:56 +0100

    fix

--- api/auth.js
+++ api/auth.js
@@ -4,7 +4,7 @@ const ctrlUser = require("../controller");
 const authMiddleware = require("../middlewares/jwt");
 
 router.post("/register", ctrlUser.register);
-router.get("/login", ctrlUser.login);
+router.post("/login", ctrlUser.login);
 router.post("/logout", authMiddleware, ctrlUser.logout);
 
 module.exports = router;
